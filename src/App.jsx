8717384f271f46a4f3de7e054c4459babd25a2ca
ravenkim <41793830+ravenkim@users.noreply.github.com>
2024-05-30 15:33:44 +0900

    [추가] 라우팅 기능

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import {ThemeProvider} from "src/assets/shadcn/ThemeProvider.jsx";
 
 import 'src/styles/shadcn.css'
+import {router} from "src/routes/router.jsx";
+import {
+    RouterProvider,
+} from "react-router-dom";
 
 function App() {
 
   return (
 
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            zzzzz
+            <RouterProvider router={router} />
         </ThemeProvider>
 
   )
