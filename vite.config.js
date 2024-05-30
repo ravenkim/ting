9abd16e568905db1fe3549783b671101e90134ac
ravenkim <41793830+ravenkim@users.noreply.github.com>
2024-05-30 13:37:27 +0900

    [추가] 테일윈드 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  //절대경로 지정
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    }
+  },
 })
