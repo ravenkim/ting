097028136233fa8bb6d18b6c24775a075497255b
ravenkim <41793830+ravenkim@users.noreply.github.com>
2024-06-07 10:20:36 +0900

    [추가] axios 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,9 @@ module.exports = {
     env: { browser: true, es2020: true, node: true },
     extends: [
         'eslint:recommended',
+        'plugin:react-hooks/recommended',
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
-        'plugin:react-hooks/recommended',
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -19,5 +19,6 @@ module.exports = {
             'warn',
             { allowConstantExport: true },
         ],
+        'react-hooks/exhaustive-deps': 'off',
     },
 }
